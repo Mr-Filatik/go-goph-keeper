8391d1b8be26285f1a0f5c4ba0998f243c5555d1
Vladislav Filatov <vladislav.filatov@mego.travel>
2025-08-21 17:35:32 +0400

    feat: update file exists func

--- internal/common/file.go
+++ internal/common/file.go
@@ -7,8 +7,19 @@ import (
 	"io/fs"
 )
 
+var errArgumentIsEmpty = errors.New("argument is empty")
+
+// GetErrArgumentIsEmpty - возвращает ошибку 'argument is empty'.
+func GetErrArgumentIsEmpty() error {
+	return errArgumentIsEmpty
+}
+
 // ExistsFS - проверяет существует ли указанный файл.
 func ExistsFS(fsys fs.FS, path string) (bool, error) {
+	if path == "" {
+		return false, fmt.Errorf("path: %w", errArgumentIsEmpty)
+	}
+
 	_, err := fs.Stat(fsys, path)
 	if err == nil {
 		return true, nil
