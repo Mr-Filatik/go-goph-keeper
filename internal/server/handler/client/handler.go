c88bfdc565774049bf9b66dede8c648ab559e178
Mr-Filatik <filatov23wlad@gmail.com>
2025-08-31 11:09:56 +0400

    feat: update client handler

--- internal/server/handler/client/handler.go
+++ internal/server/handler/client/handler.go
@@ -1,65 +1,104 @@
+// Package client предоставляет функционал для обработчиков запросов для скачивания клиента.
 package client
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"net/http"
-	"strings"
+	"time"
 
 	root "github.com/mr-filatik/go-goph-keeper"
-	"github.com/mr-filatik/go-goph-keeper/internal/common"
+	"github.com/mr-filatik/go-goph-keeper/internal/server/file"
 	"github.com/mr-filatik/go-goph-keeper/internal/server/handler"
 )
 
-// ClientHandler хранит данные необходимые для обработчиков.
-type ClientHandler struct {
+// Handler хранит данные необходимые для обработчиков.
+type Handler struct {
+	files file.IFileStorage
 	handler.Handler
 }
 
-// ClientHandlerOption представляет дополнительные опции для Handler.
-type ClientHandlerOption func(*ClientHandler)
+// HandlerOption представляет дополнительные опции для Handler.
+type HandlerOption func(*Handler)
 
-// func WithLogger(l *slog.Logger) Option { return func(h *Handler){ h.log = l } }
+// WithCustomFileStorage устанавливает кастомное файловое хранилище.
+func WithCustomFileStorage(stor file.IFileStorage) HandlerOption {
+	return func(h *Handler) {
+		h.files = stor
+	}
+}
 
 // NewHandler создаёт новый экземпляр Handler.
-func NewHandler(hand handler.Handler, opts ...ClientHandlerOption) *ClientHandler {
-	h := &ClientHandler{
+func NewHandler(hand handler.Handler, opts ...HandlerOption) *Handler {
+	clientHandler := &Handler{
 		Handler: hand,
+		files:   file.NewClientFileStorage(root.EmbedStatic),
 	}
 
-	for _, o := range opts {
-		o(h)
+	if opts == nil {
+		return clientHandler
 	}
 
-	return h
+	for index := range opts {
+		opts[index](clientHandler)
+	}
+
+	return clientHandler
 }
 
 var errUnsupportedOS = errors.New("unsupported OS")
 
+// InfoResp описывает ответ для запроса информации о доступных клиентах.
+type InfoResp struct {
+	Path    string   `json:"path"`
+	Example string   `json:"example"`
+	OS      []string `json:"os"`
+}
+
 // ClientInfo отдаёт информацию о поддержимаемых OS.
-func (h *ClientHandler) ClientInfo(writer http.ResponseWriter, _ *http.Request) {
-	info := map[string]string{
-		"download": "/client/{os}",
-		"os":       "windows, macos, linux",
-		"example":  "http://localhost:8080/client/linux",
+func (h *Handler) ClientInfo(writer http.ResponseWriter, _ *http.Request) {
+	resp := InfoResp{
+		Path:    "/client/{os}",
+		Example: "http://localhost:8080/client/linux",
+		OS: []string{
+			"linux",
+			"macos",
+			"windows",
+		},
 	}
-	h.ResponceWithJSON(writer, info)
+
+	writer.Header().Set("Content-Type", "application/json")
+
+	h.ResponceWithJSON(writer, resp)
 }
 
 // ClientDownload отдаёт бинарник CLI-клиента в зависимости от ОС.
-func (h *ClientHandler) ClientDownload(writer http.ResponseWriter, req *http.Request) {
-	os := req.PathValue("os")
+func (h *Handler) ClientDownload(writer http.ResponseWriter, req *http.Request) {
+	osParam := req.PathValue("os")
 
-	var filename, filepath, contentType string
+	path, name, err := h.files.GetFileInfo(osParam)
+	if err != nil {
+		if errors.Is(err, file.ErrUncorrectClientOS) {
+			iErr := fmt.Errorf("%s: %w", osParam, err)
+			h.ResponseError(writer, http.StatusBadRequest, iErr)
 
-	switch os {
-	case "windows":
-		filename = "client-windows.exe"
+			return
+		}
+
+		h.ResponseError(writer, http.StatusInternalServerError, err)
+
+		return
+	}
+
+	var contentType string
+
+	switch osParam {
+	case file.ClientOSWindows:
 		contentType = "application/x-msdownload"
-	case "macos":
-		filename = "client-macos.exe"
+	case file.ClientOSMacOS:
 		contentType = "application/x-mach-binary"
-	case "linux":
-		filename = "client-linux.exe"
+	case file.ClientOSLinux:
 		contentType = "application/octet-stream"
 	default:
 		h.ResponseError(writer, http.StatusBadRequest, errUnsupportedOS)
@@ -67,25 +106,21 @@ func (h *ClientHandler) ClientDownload(writer http.ResponseWriter, req *http.Req
 		return
 	}
 
-	filepath = strings.Join([]string{root.DirStatic, "/", filename}, "")
-
-	ok, err := common.ExistsFS(root.EmbedStatic, filepath)
-	if !ok {
-		if err != nil {
-			h.ResponseError(writer, http.StatusInternalServerError, err)
-
-			return
-		}
-
-		h.ResponseError(writer, http.StatusNotFound, err)
-
-		return
-	}
-
-	writer.Header().Set("Content-Disposition", "attachment; filename="+filename)
+	writer.Header().Set("Content-Disposition", "attachment; filename="+name)
 	writer.Header().Set("Content-Type", contentType)
 	writer.Header().Set("Content-Transfer-Encoding", "binary")
 	writer.Header().Set("Cache-Control", "no-cache")
 
-	http.ServeFile(writer, req, filepath)
+	// The following code does not work in tests.
+	// fullpath := strings.Join([]string{path, name}, "")
+	// http.ServeFile(writer, req, fullpath)
+
+	data, dataErr := h.files.GetFileData(path, name)
+	if dataErr != nil {
+		h.ResponseError(writer, http.StatusInternalServerError, dataErr)
+
+		return
+	}
+
+	http.ServeContent(writer, req, name, time.Time{}, bytes.NewReader(data))
 }
