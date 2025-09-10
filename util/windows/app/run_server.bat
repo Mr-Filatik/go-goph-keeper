ca52062e2b42edb65797b686101102a0dc7fc415
Mr-Filatik <filatov23wlad@gmail.com>
2025-09-10 17:10:04 +0400

    chore: add env files for project

--- util/windows/app/run_server.bat
+++ util/windows/app/run_server.bat
@@ -1,5 +1,33 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
 cd ..\..\..\
 
-go run cmd\server\main.go 
+for /f "tokens=1,* delims==" %%a in (util\env\local.env) do (
+    set %%a=%%b
+)
+
+set "ARGS="
+
+if defined CRYPTO_JWT_KEY (
+    set "ARGS=!ARGS! -crypto-jwt-key=!CRYPTO_JWT_KEY!"
+)
+
+if defined DATABASE_CONNECTION_STRING (
+    set "ARGS=!ARGS! -database=!DATABASE_CONNECTION_STRING!"
+)
+
+if defined HASH_KEY (
+    set "ARGS=!ARGS! -hash-key=!HASH_KEY!"
+)
+
+if defined SERVER_ADDRESS (
+    set "ARGS=!ARGS! -address=!SERVER_ADDRESS!"
+)
+
+go run cmd\server\main.go !ARGS!
+
+endlocal
 
 pause
